9ef22ef372eddfdb119187c5f0767d08f36fbfa8
tomek7777 <89088480+tomek7777@users.noreply.github.com>
2022-05-31 11:38:05 +0200

    db

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import http from 'http';
 import mongoose from 'mongoose';
 import { config } from './config/config';
+import Logging from './library/Logging';
 
 const router = express();
 
@@ -9,8 +10,12 @@ const router = express();
 mongoose
     .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
     .then(() => {
-        console.log('connected');
+        Logging.info('Connected to mongoDB');
+        StartServer();
     })
     .catch((error) => {
-        console.log(error);
+        Logging.error('Unable to connect: ');
+        Logging.error(error);
     });
+
+const StartServer = () => {};
